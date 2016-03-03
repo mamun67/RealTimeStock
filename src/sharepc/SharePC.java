2b6f155c6ece39575cdda69eb56acec003969cae
SK MAMUN ARFIN <mamunarfin@outlook.com>
2016-03-03 15:22:22 +0530

    Added files via upload
    Created two separate threads Producer and Consumer. Producer fetches the portfolio page and writes it to the queue and consumer  reads it and deletes it from the queue.If the queue is empty then thread is made to sleep for some time.

---
+++ src/sharepc/SharePC.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sharepc;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.WebResponse;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+/**
+ *
+ * @author mamun
+ */
+// Critical Section Peterson's Solution Solution
+public class SharePC{
+    final int maxsize=9000;
+     String[] buffer = new String[maxsize];
+     int fp=0;     
+     int rp=0;
+     int qs=0;
+     
+    
+    private class shareproducer extends Thread{ // process thread for i
+         Page page;WebClient wc;
+    String date = (new SimpleDateFormat("dd-MM-yyyy").format(Calendar.getInstance().getTime()));
+    
+    public shareproducer(WebClient webClient) throws IOException{
+        wc = webClient;
+    }
+  
+    public void insert_item(String content)
+    {
+         if (!(qs==maxsize)) {
+         qs++;
+         rp = (rp + 1)%maxsize;
+         buffer[rp] = content;
+         System.out.println("added");
+      }
+      else
+         System.err.println("Overflow\n");
+    }
+   
+    
+        public void run(){
+                try{
+                    do{
+                        int N = 0;
+                        //produce_data  produde_data();
+                        page = wc.getPage("https://in.finance.yahoo.com/portfolio/p_6/view/v1");
+                        WebResponse ws =page.getWebResponse();
+                        String content = page.getWebResponse().getContentAsString();
+                      
+                        insert_item(content);
+                                            
+                        //remainder section
+                    }while(true); // 100 is upper bound, to remove infinite looping
+                }
+                catch(Exception ex){
+                    ex.printStackTrace();
+                }
+                
+            }
+       
+        }
+    private class shareconsumer extends Thread{ // process thread for j
+        
+         public boolean buffer_empty(){
+             
+        if(qs == 0)
+        return true;
+        return false;
+    }
+         public void remove_item()
+        {
+              if (!(qs == 0)) {
+         qs--;
+         fp = (fp + 1)%maxsize;
+        // return buffer[fp];
+        System.out.println(buffer[fp]);
+         System.out.println("removed");
+      }
+      else {
+         System.err.println("Underflow");
+         }
+        }
+            public void run(){
+                try{
+                    do{
+                        if(buffer_empty())
+                            Thread.sleep(2500);
+                        remove_item();
+                      
+                                                             
+                    }while(true); // 100 is upper bound, to remove infinite looping
+                }
+                catch(Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        }
+    public SharePC(WebClient webclient) throws IOException{
+        System.out.println("Starting Threads/Processes");
+       // Thread I= new shareproducer();
+        System.out.println("producer started");
+        Thread J = new shareconsumer();
+        new shareproducer(webclient).start(); //start process i
+         System.out.println("consumer strted");
+        J.start(); //start process j
+    }
+        public static void main(String[] args) throws Exception{
+            
+          try{
+             WebClient webClient = new WebClient(BrowserVersion.FIREFOX_38); //Initiate a WebClient variable.  
+               login lgn = new login();
+               try{
+               System.out.println("before login");
+               webClient = lgn.tremorLogin(webClient);
+               }
+               catch (Exception x) {
+                x.printStackTrace();
+            }
+             System.out.println("in main");
+            SharePC cSec = new SharePC(webClient);
+          }
+                catch (Exception x) {
+                x.printStackTrace();
+            }
+    }
+}
